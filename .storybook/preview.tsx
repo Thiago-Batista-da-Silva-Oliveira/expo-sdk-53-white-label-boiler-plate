3ed7659958c14361e73256e782b79d0009dcb062
Thiago <gmoliveira69@gmail.com>
2025-08-13 15:52:13 -0300

    .

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,23 +1,11 @@
 import type { Preview } from '@storybook/react-native';
 import React from 'react';
-import { ThemeProvider } from 'styled-components/native';
-import { lightTheme } from '../src/theme/light';
-
-// Mock environment
-jest.mock('@/config/env', () => ({
-  env: {
-    EXPO_PUBLIC_API_URL: 'https://storybook-api.com',
-    EXPO_PUBLIC_SOCKET_URL: 'https://storybook-socket.com',
-    EXPO_PUBLIC_ENVIRONMENT: 'test',
-    EXPO_PUBLIC_GOOGLE_MAPS_KEY: 'storybook_key',
-    EXPO_PUBLIC_BRAND: 'default1',
-  },
-}));
+import { ThemeProvider } from '../src/theme/ThemeProvider';
 
 const preview: Preview = {
   decorators: [
-    (Story) => (
-      <ThemeProvider theme={lightTheme}>
+    (Story: any) => (
+      <ThemeProvider>
         <Story />
       </ThemeProvider>
     ),
